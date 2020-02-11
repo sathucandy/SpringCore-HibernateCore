d2e736803f9fefc211d1ffebc09720149ae73bd8
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-02-11 19:21:36 +0530

    Create an Aspect with @Before advice annotation

---
+++ 11-spring-demp-aop/src/com/sarthak/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,19 @@
+package com.sarthak.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+	// this is where we add all our related advises for logging
+
+	// lets start with an @Before advice
+
+	@Before("execution(public void addAccount())")
+	public void beforeAddAccountAdvice() {
+		System.out.println("\n======> Executin @Before advice on addAccount() <======");
+	}
+
+}
