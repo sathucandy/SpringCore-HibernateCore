2bb04e202335fd06b86cf0ac5993e4e2dcdb5095
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-02-21 16:03:06 +0530

    Modifying aroundgetFortune() method

--- 18-spring-demo-aop-zz-after-around/src/com/sarthak/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 18-spring-demo-aop-zz-after-around/src/com/sarthak/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -20,10 +20,30 @@ import com.sarthak.aopdemo.Account;
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
-	
+
 	@Around("execution(* com.sarthak.aopdemo.service.*.getFortune(..))")
-	public Object aroundgetFortune(ProceedingJoinPoint theProceedingJoinPoint) throws Throwable{
+	public Object aroundgetFortune(ProceedingJoinPoint theProceedingJoinPoint) throws Throwable {
+
+		// print out which method we are advising on
+		String method = theProceedingJoinPoint.getSignature().toShortString();
+		System.out.println("\n======> Executing @Around on method: " + method);
+
+		// get begin timestamp
+		long begin = System.currentTimeMillis();
+
+		// now let's execute the method
+
+		Object result = theProceedingJoinPoint.proceed();
+
+		// get end timestamp
+		long end = System.currentTimeMillis();
+
+		// compute duration and display it
+		long duration = end - begin;
+		System.out.println("\n=========> Duration: " + duration / 1000.0 + "seconds");
 		
+		return result;
+
 	}
 
 	@After("execution(* com.sarthak.aopdemo.dao.AccountDAO.findAccounts(..))")
