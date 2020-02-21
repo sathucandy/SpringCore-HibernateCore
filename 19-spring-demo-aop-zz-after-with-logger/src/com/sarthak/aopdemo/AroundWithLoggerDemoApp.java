d5a94f688310e20121df15374d39903c27d9f3f6
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-02-21 17:10:53 +0530

    Implementing Logger class in AroundWithLoggerDemoApp.class

--- 19-spring-demo-aop-zz-after-with-logger/src/com/sarthak/aopdemo/AroundWithLoggerDemoApp.java
+++ 19-spring-demo-aop-zz-after-with-logger/src/com/sarthak/aopdemo/AroundWithLoggerDemoApp.java
@@ -1,6 +1,7 @@
 package com.sarthak.aopdemo;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -8,6 +9,8 @@ import com.sarthak.aopdemo.dao.AccountDAO;
 import com.sarthak.aopdemo.service.TrafficFortuneService;
 
 public class AroundWithLoggerDemoApp {
+
+	private static Logger myLogger = Logger.getLogger(AroundWithLoggerDemoApp.class.getName());
 
 	public static void main(String[] args) {
 
@@ -17,13 +20,18 @@ public class AroundWithLoggerDemoApp {
 		// get the bean from spring container
 		TrafficFortuneService fortuneService = context.getBean("trafficFortuneService", TrafficFortuneService.class);
 
-		System.out.println("\nMain program demo app");
+//		System.out.println("\nMain program demo app");
+		myLogger.info("\nMain program demo app");
 
-		System.out.println("Calling getFortune");
+//		System.out.println("Calling getFortune");
+		myLogger.info("Calling getFortune");
 
 		String data = fortuneService.getFortune();
-		System.out.println("\nMy fortune is: " + data);
-		System.out.println("Finished");
+//		System.out.println("\nMy fortune is: " + data);
+		myLogger.info("\nMy fortune is: " + data);
+
+//		System.out.println("Finished");
+		myLogger.info("Finished\"");
 
 		// close the context
 		context.close();
