92eee557d823b3e6097099f518a72cf42aa361ec
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-02-27 07:52:00 +0530

    Using inMemoryAuthentication

--- 23-spring-basic-security-2/src/main/java/com/sarthak/springsecurity/demo/config/DemoSecurityConfig.java
+++ 23-spring-basic-security-2/src/main/java/com/sarthak/springsecurity/demo/config/DemoSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
@@ -11,8 +13,12 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(auth);
+		// add users for in memory authentication
+
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		auth.inMemoryAuthentication().withUser(users.username("John").password("123").roles("Employee"))
+				.withUser(users.username("Mary").password("456").roles("Manager"))
+				.withUser(users.username("Susan").password("789").roles("Employee"));
 	}
 
 }
