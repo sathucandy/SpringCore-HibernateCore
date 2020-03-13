e652ef78310f4e7486411009bb74091399114064
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-03-13 21:14:31 +0530

    Creating driver class

---
+++ 29-jackson-databinding-json-demo/src/main/java/com/sarthak/jackson/json/demo/Driver.java
@@ -0,0 +1,22 @@
+package com.sarthak.jackson.json.demo;
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+		try {
+
+			// create object mapper
+
+			// read JSON file and map/convert to Java POJO:
+			// data/sample-lite.json
+
+			// print first name and last name
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
