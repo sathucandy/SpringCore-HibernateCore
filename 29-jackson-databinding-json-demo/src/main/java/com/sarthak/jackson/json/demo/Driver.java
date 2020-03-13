1035f8e6fd2cc5748048fd829dbb263fedfa625f
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-03-13 21:30:58 +0530

    Binding json to pojo

--- 29-jackson-databinding-json-demo/src/main/java/com/sarthak/jackson/json/demo/Driver.java
+++ 29-jackson-databinding-json-demo/src/main/java/com/sarthak/jackson/json/demo/Driver.java
@@ -1,5 +1,9 @@
 package com.sarthak.jackson.json.demo;
 
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Driver {
 
 	public static void main(String[] args) {
@@ -7,11 +11,15 @@ public class Driver {
 		try {
 
 			// create object mapper
+			ObjectMapper mapper = new ObjectMapper();
 
 			// read JSON file and map/convert to Java POJO:
 			// data/sample-lite.json
+			Student theStudent = mapper.readValue(new File("data/sample-lite.json"), Student.class);
 
 			// print first name and last name
+			System.out.println("First Name = " + theStudent.getFirstName());
+			System.out.println("Last Name = " + theStudent.getLastName());
 
 		} catch (Exception e) {
 			e.printStackTrace();
