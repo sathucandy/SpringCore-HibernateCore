d7fcdb9ac991645019289918c53cd2000613d82b
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-03-14 20:41:25 +0530

    Creating POJO student

---
+++ 30-spring-rest-demo/src/main/java/com/sarthak/springdemo/entity/Student.java
@@ -0,0 +1,18 @@
+package com.sarthak.springdemo.entity;
+
+public class Student {
+
+	private String firstName;
+	private String lastName;
+
+	public Student() {
+
+	}
+
+	public Student(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+}
